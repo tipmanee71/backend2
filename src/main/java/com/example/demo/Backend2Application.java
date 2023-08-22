18a36a9c8a506b69b279a3fe166c0beab016ac69
Tipmanee71 <135458090+tipmanee71@users.noreply.github.com>
2023-08-23 05:07:51 +0700

    1

---
+++ src/main/java/com/example/demo/Backend2Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend2Application.class, args);
+		System.out.println("Hello Spring");
+		System.out.println("Spring Boot");
+	}
+
+}
